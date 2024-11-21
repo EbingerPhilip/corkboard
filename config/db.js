0ac55bc240daaa0363adb941056c8dfe6f1fa3e5
EbingerPhilip <philip.ebinger@gmx.at>
2024-11-21 15:21:37 +0100

    access to DB in azure

--- config/db.js
+++ config/db.js
@@ -1,11 +1,27 @@
 require("dotenv").config();
 const mysql = require('mysql2')
 
+
+/* //localhost connection
 const pool = mysql.createPool({
     host:'localhost',
     user:'root',
-    database:'users-cb',
     password:'#corkboard',
+    database:'users-cb',  
+    
+})*/
+
+
+//server connection 
+console.log(process.env.DBuser + " : " + process.env.DBpassword);
+
+
+const pool = mysql.createPool({
+    host: '172.205.225.192' ,
+    user: process.env.DBuser,
+    password:process.env.DBpassword,
+    database:'users_cb',  
+    
 })
 
 module.exports = pool.promise();
